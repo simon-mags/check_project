276159fc5182d17f9a7469913eb324f8d41164af
root <root@localhost>
2023-07-05 01:03:48 +0930

    move to multiple values for ignore lists

--- roles/diff_report_files/library/compare_files.py
+++ roles/diff_report_files/library/compare_files.py
@@ -2,65 +2,56 @@ from ansible.module_utils.basic import AnsibleModule
 import json
 import subprocess
 import re
+from deepdiff import DeepDiff
+import traceback
 
-def install_dependencies():
-    # Install required dependencies using pip
-    try:
-        subprocess.check_call(["pip", "install", "deepdiff"])
-    except subprocess.CalledProcessError as e:
-        raise Exception(f"Failed to install dependencies: {e}")
+def convert_to_string(item):
+    # Convert byte strings to string
+    if isinstance(item, bytes):
+        return item.decode('utf-8')
+    return str(item)
 
-def compare_files():
-    # Install dependencies before importing them
-    install_dependencies()
+# Custom Python module
+def compare_files(module, before_file, after_file, ignore_whole_lines, ignore_regex_patterns):
+    # Convert AnsibleUnicode to regular Python strings
+    ignore_whole_lines = [str(line) for line in ignore_whole_lines]
+    ignore_regex_patterns = [str(pattern) for pattern in ignore_regex_patterns]
 
-    # Import necessary modules
-    from deepdiff import DeepDiff
-
-    # Define the expected module arguments
-    module_args = dict(
-        before_file=dict(type='str', required=True),
-        after_file=dict(type='str', required=True),
-        ignore_list=dict(type='list', elements='str')
-    )
-
-    # Create an AnsibleModule instance with the provided arguments
-    module = AnsibleModule(
-        argument_spec=module_args,
-        supports_check_mode=True
-    )
-
-    # Retrieve the values of the module arguments
-    before_file = module.params['before_file']
-    after_file = module.params['after_file']
-    ignore_list = module.params['ignore_list']
+    # Print the data types and values of ignore_whole_lines and ignore_regex_patterns
+    print(f"ignore_whole_lines - type: {type(ignore_whole_lines)}, value: {ignore_whole_lines}")
+    print(f"ignore_regex_patterns - type: {type(ignore_regex_patterns)}, value: {ignore_regex_patterns}")
 
     try:
         # Load the JSON data from the before and after files
         with open(before_file, 'r') as f:
             before_data = json.load(f)
+            print(f"Loaded data from before_file: {before_data}")
+
         with open(after_file, 'r') as f:
             after_data = json.load(f)
+            print(f"Loaded data from after_file: {after_data}")
 
         # Create a list to store ignored paths
         ignored_paths = []
 
-        # Iterate over each item in the ignore_list
-        for ignore_item in ignore_list:
-            if ignore_item.startswith('/'):
-                # If the item starts with '/', consider it a whole line to ignore
-                ignored_paths.append(ignore_item)
-            else:
-                # If it doesn't start with '/', treat it as a regular expression pattern
-                # regex_pattern = re.escape(ignore_item)
-                regex_pattern = ignore_item.replace(r"\.", ".")
-                # Find paths matching the regex pattern and add them to ignored_paths
-                paths_to_ignore = [
-                    path
-                    for path in DeepDiff(before_data, after_data, view='tree')
-                    if re.search(regex_pattern, path)
-                ]
-                ignored_paths.extend(paths_to_ignore)
+        # Check if ignore_whole_lines is empty or contains only one data type
+        if ignore_whole_lines:
+            # Add whole lines to ignored_paths
+            ignored_paths.extend(ignore_whole_lines)
+
+        # Check if ignore_regex_patterns is empty or contains only one data type
+        if ignore_regex_patterns:
+            # Add regex patterns to ignored_paths
+            regex_patterns = [re.escape(pattern) for pattern in ignore_regex_patterns]
+            paths_to_ignore = [
+                path
+                for path in DeepDiff(before_data, after_data, view='tree')
+                for pattern in regex_patterns
+                if re.search(pattern, path)
+            ]
+            ignored_paths.extend(paths_to_ignore)
+
+        print(f"Types in ignored_paths: {[type(item) for item in ignored_paths]}")
 
         # Use the ignored_paths in the DeepDiff call to exclude them from the comparison
         differences = DeepDiff(before_data, after_data, exclude_paths=ignored_paths)
@@ -69,8 +60,8 @@ def compare_files():
         result = {
             'before_file': before_file,
             'after_file': after_file,
-            'ignore_list': ignore_list,
-            'ignored_paths': ignored_paths,
+            'ignore_whole_lines': ignore_whole_lines,
+            'ignore_regex_patterns': ignore_regex_patterns,
             'differences': differences.to_dict()
         }
 
@@ -78,12 +69,32 @@ def compare_files():
         module.exit_json(changed=False, result=result)
 
     except Exception as e:
+        # Print the full traceback
+        traceback.print_exc()
         # If any exception occurs, fail the module and return the error message
         module.fail_json(msg=str(e))
 
 def main():
+    # Define the expected module arguments
+    module_args = dict(
+        before_file=dict(type='str', required=True),
+        after_file=dict(type='str', required=True),
+        ignore_whole_lines=dict(type='list', elements='str'),
+        ignore_regex_patterns=dict(type='list', elements='str')
+    )
+
+    # Create an AnsibleModule instance with the provided arguments
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
     # Call the compare_files function to execute the module logic
-    compare_files()
+    compare_files(module,
+                  module.params['before_file'],
+                  module.params['after_file'],
+                  module.params['ignore_whole_lines'],
+                  module.params['ignore_regex_patterns'])
 
 if __name__ == '__main__':
     # Invoke the main function when the script is run directly
